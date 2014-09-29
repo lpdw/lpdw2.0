ee0970ed0c745d554fa53a5d2596149f522212cf
Thomas Maurer <thomas.wellan@gmail.com>
2014-09-29 11:20:32 +0200

    Add control access to Admin area

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
+	#Before any action just authetificate user
+	before_action :is_admin
+
 		def create_user
 		@user = User.new
 	end
@@ -6,4 +9,13 @@ class AdminController < ApplicationController
 	def new
 		@user = User.new(params[:user].permit(:email, :password, :password_confirmation, :role))
     end
+
+    def is_admin
+    	@user = current_user
+    if (@user.role != "Admin") then
+      flash[:error] = "You must be admin to access this section"
+      redirect_to root_path # halts request cycle
+    end
+  end
+
 end
