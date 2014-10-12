2914bbe3930756b84f421f954b7425dd4fdf0c2e
Alain Cajuste <cajuste.alain@gmail.com>
2014-10-12 03:21:38 +0200

    Fix assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Lpdw20
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    config.serve_static_assets = true
   end
 end
