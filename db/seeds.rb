5e1e478c6724e4f8452c92a70d847f1a4dbcd4ae
Thomas Maurer <thomas.wellan@gmail.com>
2014-10-15 09:07:56 +0200

    Add default admin in db/seed.rb for easier test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(:email => 'admin@mail.com', :password => 'azerty123', :password_confirmation => 'azerty123', :role =>'admin')
