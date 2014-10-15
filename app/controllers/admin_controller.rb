c66c7809df1f677194b6766d36bc835b3039bdbe
Walid <w.yahiacherif@gmail.com>
2014-10-15 15:20:19 +0200

    modif admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -23,6 +23,11 @@ class AdminController < ApplicationController
 
 
 # actuality Controller
+  before_action :get_this,only: [:edit_actuality,:delete_actuality]
+  def get_this
+    @this = Actuality.find(params[:id])  
+  end
+
   def create_actuality
     @actuality = Actuality.new
   end
@@ -30,6 +35,12 @@ class AdminController < ApplicationController
   def new_actuality
     @actuality = Actuality.new(params[:actuality].permit(:title, :content, :author))
   end
- 
+  
+  def edit_actuality
+     
+  end
+  def delete_actuality
+    @this.destroy
+  end
 
 end
