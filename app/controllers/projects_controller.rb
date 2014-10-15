078fc2ff5dc537e346eaace0bb356838c85c68b4
Walid <w.yahiacherif@gmail.com>
2014-10-15 12:14:24 +0200

    admin controller modification , addition of Projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,19 @@
 class ProjectsController < ApplicationController
   
-  def new
+  def new_projects
   	@Projects = Projects.new
   end
 
-  def create_actuality
+  def create_projects
 
   end
 
-  def delete_actuality
+  def delete_aprojects
   	@Projects = projects.find()
   	@Projects.destroy
   end
 
-  def  update_actuality
+  def  update_projects
   	@Projects = projects.find()
   	
   	@Projects.save
