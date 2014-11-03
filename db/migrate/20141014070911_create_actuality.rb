6bde88b7f4ed171d3042e017868edab88d1afcad
Alain Cajuste <cajuste.alain@gmail.com>
2014-11-03 09:12:08 +0100

    Fix table actuality

--- db/migrate/20141014070911_create_actuality.rb
+++ db/migrate/20141014070911_create_actuality.rb
@@ -2,7 +2,7 @@ class CreateActuality < ActiveRecord::Migration
   def change
     create_table :actualities do |t|
     	t.string :title,              null: false, default: ""
-      	t.string :content, null: false, default: ""
+      	t.text :content, null: false, default: ""
       	t.string :author, null: false, default: ""
     end
   end
