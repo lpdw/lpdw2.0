e3d8033eca3a213a3f71863e23a59d0e60e5fe38
Thomas Maurer <wellan78@hotmail.fr>
2015-01-08 10:11:32 +0000

    Fix Tinymce DB image

--- db/schema.rb
+++ db/schema.rb
@@ -29,10 +29,13 @@ ActiveRecord::Schema.define(version: 20150107184254) do
   end
 
   create_table "images", force: true do |t|
-    t.string   "alt"
-    t.string   "attachment"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+   t.string   "alt"
+   t.string   "file_file_name"
+   t.string   "file_content_type"
+   t.integer  "file_file_size"
+   t.datetime "file_updated_at"
+   t.datetime "created_at"
+   t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
