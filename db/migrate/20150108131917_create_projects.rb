c65034f1e5ad454573a51fd9638b1de57aad8a70
Thomas Maurer <wellan78@hotmail.fr>
2015-01-30 09:55:28 +0000

    Add image upload to project Fix edit + remove useless photo column

--- db/migrate/20150108131917_create_projects.rb
+++ db/migrate/20150108131917_create_projects.rb
@@ -1,7 +1,6 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :photo
       t.string :name
       t.text :description
       t.string :link
