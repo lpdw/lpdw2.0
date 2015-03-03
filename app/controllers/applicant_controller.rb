be341f8c093fc0c0ca4285c734fec3cd49cbf0b3
Thomas Maurer <wellan78@hotmail.fr>
2015-03-03 19:04:11 +0000

    Customize form 2 for apply

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -20,7 +20,7 @@ class ApplicantController < ApplicationController
   #Post method apply
   def applicant_create_apply
     @applicant = Applicant.find_by_assurance(params[:assurance].to_s)
-    if (@applicant.blank?)
+    if @applicant.blank?
       flash["error"] = "Connectez vous pour modifier votre dossier d'application"
       redirect_to '/postuler'
     else
@@ -30,12 +30,30 @@ class ApplicantController < ApplicationController
     flash["error"] = "Connectez vous pour modifier votre dossier d'application"
     redirect_to '/postuler'
   end
-  def applicant_create_apply_from_scratch
-    @applicant = Applicant.new(params[:applicant].permit(:deposit_date, :name, :first_name, :zip_code, :city, :home_phone, :private_phone, :email, :birth, :birth_place, :nationality, :assurance, :status, :has_connection, :connection_desc, :know_formation, :english_skill, :other_language, :after_school, :ip_address, :created_at, :updated_at, :address, :other_application_attributes))
+  def create_apply
+    @applicant = Applicant.new(params[:applicant].permit(:deposit_date, :name, :first_name, :zip_code, :city, :home_phone, :private_phone, :email, :birth, :birth_place, :nationality, :assurance, :status, :has_connection, :connection_desc, :know_formation, :english_skill, :other_language, :after_school, :ip_address, :created_at, :updated_at, :address, other_application_attributes: [:id_applicant, :content, :created_at, :updated_at], cursus: [:graduation, :year, :option, :resut, :place]))
     if @applicant.save
       flash["success"] = "Dossier sauvegardé"
+      redirect_to '/postuler/'+ @applicant.assurance.to_s
     else
       flash["error"] = "Erreur d'enregistrement"
+      redirect_to '/postuler'
     end
+  end
+  def update_apply
+    @applicant = Applicant.find_by_assurance(params[:assurance].to_s)
+
+    @applicant.update(params[:applicant].permit(:english_skill, :after_school, :other_language, cursus_attributes: [:graduation, :year, :option, :result, :place]))
+    if @applicant.save
+      flash["success"] = "Dossier sauvegardé"
+      redirect_to '/postuler/'+ @applicant.assurance.to_s
+    else
+      flash["error"] = "Erreur d'enregistrement"
+      redirect_to '/postuler'
+    end
+  end
+  def update_cursus_apply
+    @applicant = Applicant.find_by_assurance(params[:assurance].to_s)
+    @cursus = @applicant.cursus.where(id_applicant: @applicant.id, option: params[:applicant][:cursus][], result: @applicant.cursus.year, year: @applicant.cursus.year, place: @applicant.cursus.year).first_or_create
   end
 end
