7c8005d02b1a50db90824b1d8639edf751af6ea9
Ivan <ivan.daum@gmail.com>
2015-03-10 20:27:25 +0000

    feat(intervenant_admin): restriction OK for intervenants

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,21 @@
 class SessionsController < Devise::SessionsController
 	# POST /resource/sign_in
-  def create
-  	@user = User.find_for_database_authentication(email:sign_in_params[:email])
-  	if @user && @user.role === "admin"
-  		self.resource = warden.authenticate!(auth_options)
-	    set_flash_message(:notice, :signed_in) if is_flashing_format?
-	    sign_in(resource_name, resource)
-	    yield resource if block_given?
-	    respond_with resource, location: after_sign_in_path_for(resource)
-    else
-      flash["error"] = "You must be admin to access this section"
-      redirect_to :back # halts request cycle
-  end
-end
-
+	def create
+		@user = User.find_for_database_authentication(email:sign_in_params[:email])
+		if @user
+			if @user.role === "admin" || @user.role === "intervenant"
+				self.resource = warden.authenticate!(auth_options)
+				set_flash_message(:notice, :signed_in) if is_flashing_format?
+				sign_in(resource_name, resource)
+				yield resource if block_given?
+				respond_with resource, location: after_sign_in_path_for(resource)
+			else
+				flash["error"] = "You must be admin or intervenant to access this section"
+				redirect_to :back # halts request cycle
+			end
+		else
+			flash["error"] = "You must be admin or intervenant to access this section"
+			redirect_to :back # halts request cycle
+		end
+	end
 end
