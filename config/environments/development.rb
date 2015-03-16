8c64f900b59341c5634edede3cbd820f557c0aaa
Alain Cajuste <cajuste.alain@gmail.com>
2015-03-16 09:24:22 +0100

    feat(map): send email contact

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,30 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  config.action_mailer.perform_deliveries = true
+  # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'localhost:3000',
+  #   :user_name            => "lpdw2.0@gmail.com",
+  #   :password             => "azerty92",
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
+  # SMTP settings for mandrill
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mandrillapp.com",
+    :port                 => 587,
+    :domain               => 'localhost:3000',
+    :user_name            => "lpdw2.0@gmail.com",
+    :password             => "Ai3uyjuFNEuLgA3ckNI4Yw"
+  }
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
