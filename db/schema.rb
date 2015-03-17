7976fd1bbd52fe1d36da820e52c219214c0d89ce
Thomas Maurer <thomas.wellan@gmail.com>
2015-03-17 20:37:09 +0100

    Applicant :: Add hash to attachedFile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317135800) do
+ActiveRecord::Schema.define(version: 20150317104038) do
 
   create_table "actualities", force: true do |t|
     t.string   "title"
