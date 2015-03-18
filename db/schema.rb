34d49a43587628b7e65d7c8375e81a1ece4f331b
Ivan <ivan.daum@gmail.com>
2015-03-18 16:07:51 +0000

    fix(applicants): js for option admin

--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,16 @@ ActiveRecord::Schema.define(version: 20150318141325) do
 
   add_index "cursus", ["id_applicant"], name: "cursus_id_applicant_fk", using: :btree
 
+  create_table "images", force: true do |t|
+    t.string   "alt"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "options", force: true do |t|
     t.string "key"
     t.string "value"
@@ -135,6 +145,7 @@ ActiveRecord::Schema.define(version: 20150318141325) do
   add_index "project_applicants", ["id_applicant"], name: "project_applicants_id_applicant_fk", using: :btree
 
   create_table "projects", force: true do |t|
+    t.string   "photo"
     t.string   "name"
     t.text     "description"
     t.string   "link"
