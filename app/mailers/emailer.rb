6b541d63d862e0311481d8369bd8c529d4811d2f
Alain Cajuste <cajuste.alain@gmail.com>
2015-03-18 10:09:54 +0100

    feat and style(interview): config prod and style mail

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -3,10 +3,11 @@ class Emailer < ActionMailer::Base
 
    def contact(data, sent_at = Time.now)
       @name = data["name_contact"]
-      @recipient = data["email_contact"]
+      @recipient = "l.ricard@faclab.org"
       @message = data["message_contact"]
-      mail(to: @recipient, subject: 'Contact')
+      mail(to: @recipient, subject: 'Site LP: Contact')
    end
+
    def send_mail_interview(email,date, sent_at = Time.now)
     @date = date
     mail(to: email, subject: 'LP : Entretien de motivation')
