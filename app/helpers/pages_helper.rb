c7643912c1098bc77c54ba57fe7f4e1acdf21e4a
Ivan <ivan.daum@gmail.com>
2015-03-18 19:08:03 +0000

    merge(all): commit before merge

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,14 +1,18 @@
 module PagesHelper
     def on_show_route_apply
 
-    option = Option.find_by(key: "setting_route_apply")
-        case option.value
-        when "true"
-            @on_show_route_apply = true
-        when "false"
-            @on_show_route_apply = false
+        option = Option.find_by(key: "setting_route_apply")
+        if option != nil
+            case option.value
+            when "true"
+                @on_show_route_apply = true
+            when "false"
+                @on_show_route_apply = false
+            else
+                @on_show_route_apply = value
+            end
         else
-            @on_show_route_apply = value
+            @on_show_route_apply = false
         end
     end
 end
