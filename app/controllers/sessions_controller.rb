fe0f3a87392909aae7796ff7d183302cbab2b31a
thomas maurer <toto-ange@hotmail.fr>
2015-04-13 10:51:39 +0200

    fix(syntax) & prod

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < Devise::SessionsController
 				set_flash_message(:notice, :signed_in) if is_flashing_format?
 				sign_in(resource_name, resource)
 				yield resource if block_given?
-				respond_with resource, location: after_sign_in_path_for(resource)
+				redirect_to '/admin/'
       elsif @user.role === "applicant"
         @applicant = Applicant.find_by(:id => @user.id_applicant)
         self.resource = warden.authenticate!(auth_options)
