be1485cd7b84f0e6f3c3d97d28e78c90bbcc5f7a
thomas maurer <toto-ange@hotmail.fr>
2015-09-30 16:59:23 +0200

    Current production state

--- public/assets/tinymce/plugins/uploadimage/langs/es.js
+++ public/assets/tinymce/plugins/uploadimage/langs/es.js
@@ -1,11 +1,1 @@
-tinyMCE.addI18n('es', {
-  'Insert an image from your computer': 'Insertar una imagen desde su computadora',
-  'Insert image': 'Insertar una imagen',
-  'Choose an image': "Elija una imagen",
-  'You must choose a file': "Debe elegir un archivo",
-  'Got a bad response from the server': "Mala respuesta del servidor",
-  "Didn't get a response from the server": "No se recibió respuesta del servidor",
-  'Insert': "Insertar",
-  'Cancel': "Cancelar",
-  'Image description': "Descripción de la imagen",
-});
+tinyMCE.addI18n("es",{"Insert an image from your computer":"Insertar una imagen desde su computadora","Insert image":"Insertar una imagen","Choose an image":"Elija una imagen","You must choose a file":"Debe elegir un archivo","Got a bad response from the server":"Mala respuesta del servidor","Didn't get a response from the server":"No se recibi\xf3 respuesta del servidor",Insert:"Insertar",Cancel:"Cancelar","Image description":"Descripci\xf3n de la imagen"});
--- public/assets/tinymce/plugins/uploadimage/langs/fr.js
+++ public/assets/tinymce/plugins/uploadimage/langs/fr.js
@@ -1,11 +1,1 @@
-tinyMCE.addI18n('fr', {
-  'Insert an image from your computer': "Envoyer une image de votre ordinateur",
-  'Insert image': "Insérer une image",
-  'Choose an image': "Choisissez une image",
-  'You must choose a file': "Vous devez sélectionner un fichier",
-  'Got a bad response from the server': "Le serveur a envoyé une réponse erronée",
-  "Didn't get a response from the server": "Le serveur n'a pas renvoyé de réponse",
-  'Insert': "Insérer",
-  'Cancel': "Annuler",
-  'Image description': "Description de l'image",
-});
+tinyMCE.addI18n("fr",{"Insert an image from your computer":"Envoyer une image de votre ordinateur","Insert image":"Ins\xe9rer une image","Choose an image":"Choisissez une image","You must choose a file":"Vous devez s\xe9lectionner un fichier","Got a bad response from the server":"Le serveur a envoy\xe9 une r\xe9ponse erron\xe9e","Didn't get a response from the server":"Le serveur n'a pas renvoy\xe9 de r\xe9ponse",Insert:"Ins\xe9rer",Cancel:"Annuler","Image description":"Description de l'image"});
