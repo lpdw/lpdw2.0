5819c395c44f23a24adb80d09faa7240f07e8b11
Maxime Florile <florile.maxime@gmail.com>
2015-10-02 18:53:25 +0200

    Fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408080604) do
+ActiveRecord::Schema.define(version: 20151001151850) do
 
   create_table "actualities", force: true do |t|
     t.string   "title"
