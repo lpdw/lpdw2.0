3e48b745f808f1960b6189be6c89b00ac7bd17fd
Maxime Florile <florile.maxime@gmail.com>
2015-10-02 18:50:48 +0200

    Fix merge

--- config/routes.rb
+++ config/routes.rb
@@ -90,6 +90,11 @@ root 'pages#home'
   patch '/postuler/:assurance', :to => 'applicant#update_apply', :as => 'update_apply'
   get '/mentions', :to => 'pages#mentions'
   get '/live', :to => 'pages#live'
+<<<<<<< Updated upstream
   get '/studentsbook', :to => 'student#show'
   get '/studentprofil', :to => 'student#profil'
+=======
+  get '/studentbook', :to => 'student#show'
+  get '/studentprofil', :to => 'student#profil'
+>>>>>>> Stashed changes
 end
