2758dd1736895abc60bd69a21f5e0e891e972413
Hema.Lambert <lamberthema@gmail.com>
2015-10-02 09:59:35 +0200

    new Student controller

--- app/controllers/student_controller.rb
+++
@@ -1,9 +0,0 @@
-class StudentController < ApplicationController
-
-  def new
-    @actuality = Actuality.find(params[:id])
-  end
-  def edit(id)
-    @actuality = Actuality.find(params[:id])
-  end
-end
