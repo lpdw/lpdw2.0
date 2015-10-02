47deb75a48b2a45e3361307272d3fc9afbe7039f
Maxime Florile <florile.maxime@gmail.com>
2015-10-02 12:29:22 +0200

    Fix merge

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -3,4 +3,10 @@ class StudentController < ApplicationController
     @title_admin = "Utilisateurs"
     @student = Student.joins(:user).select("*")
   end
+
+  def profil
+    # @user = User.find(params[:id], :include => :Users_infos)
+    @user = User.find(params[:id], :include => [:users_infos => :current_job_title], :conditions => ['users_infos.user_id = ?', params[:id]])
+
+  end
 end
