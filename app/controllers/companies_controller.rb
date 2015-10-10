4adb64fb1c6b3f34ef49956148c6c89e031771cc
Eraac <kevin@labesse.me>
2015-10-10 08:18:13 +0200

    CRUD entreprise

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,5 @@
 class CompaniesController < ApplicationController
-  before_action :authenticate_user!, :is_admin
+  before_action :is_admin
   layout "_admin"
 
   def is_admin
@@ -17,18 +17,47 @@ class CompaniesController < ApplicationController
 
   def create_company
     @title_admin = "Entreprise"
-    @companies = Companies.new
+    @company = Companies.new
+  end
+
+  def new_company
+    @title_admin = "Entreprise"
+    @company = Companies.new(params[:companies].permit(:name, :description, :address, :zip_code, :city, :phone_number, :website, :twitter, :linkedin))
+    if @company.save
+      flash["sucess"] = "Company created"
+      redirect_to admin_show_companies_path()
+    else
+      flash["fail"] = "Company not created"
+      redirect_to admin_create_company_path()
+    end
   end
 
   def edit_company
+    @title_admin = "Entreprise"    
+    @company = Companies.find(params[:id])
+  end
+
+  def update_company
+    @title_admin = "Entreprise"
+    @company = Companies.find(params[:id])
+
+    if @company.update_attributes(params[:company].permit(:name, :description, :address, :zip_code, :city, :phone_number, :website, :twitter, :linkedin))
+      flash["sucess"] = "Company edited"
+      redirect_to admin_show_companies_path
+    else
+      redirect_to admin_edit_company_path(company)
+    end
   end
 
   def delete_company
-  end
-
-  def show_company
-    @title_admin = "Entreprise"
     @company = Companies.find(params[:id])
+    if @company.destroy
+      flash["sucess"] = "Company deleted"
+      redirect_to admin_show_companies_path()
+    else
+      flash[:error] = "Company not deleted"
+      redirect_to admin_show_companies_path()
+    end    
   end
 
 end
