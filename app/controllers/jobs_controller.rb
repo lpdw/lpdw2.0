2f541d19fa24e047c09010fc4ea161fe6b8e8241
Eraac <kevin@labesse.me>
2015-10-10 08:18:36 +0200

    CRUD offre d'emploi

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -22,19 +22,42 @@ class JobsController < ApplicationController
 
   def new_job
     @title_admin = "Offre d'emploi"
-    @job = Jobs.new()
+    @job = Jobs.new(params[:jobs].permit(:job_name, :job_number, :job_description, :start_at, :duration, :profil, :contact, :location, :skills))
+    if @job.save
+      flash["sucess"] = "Job created"
+      redirect_to admin_show_jobs_path
+    else
+      flash["fail"] = "Job not created"
+      redirect_to admin_create_jobs_path
+    end
   end
 
   def edit_job
-    
+   @title_admin = "Offre d'emploi"
+   @job = Jobs.find(params[:id]) 
   end
 
   def update_job
-    
+    @title_admin = "Offre d'emploi"
+    @job = Jobs.find(params[:id])
+
+    if @job.update_attributes(params[:job].permit(:job_name, :job_number, :job_description, :start_at, :duration, :profil, :contact, :location, :skills))
+      flash["sucess"] = "Job updated"
+      redirect_to admin_show_jobs_path
+    else
+      redirect_to admin_edit_jobs_path(job)
+    end
   end
 
   def delete_job
-    
+    @job = Jobs.find(params[:id])
+    if @job.destroy
+      flash["sucess"] = "Job deleted"
+      redirect_to admin_show_jobs_path
+    else
+      flash[:error] = "Job not deleted"
+      redirect_to admin_show_jobs_path
+    end
   end
 
 end
