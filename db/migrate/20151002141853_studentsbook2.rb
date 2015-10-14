4ab43dc0765c19ea1b520b1063cde89506be2e8e
Laouiti Elias Cédric <elias@laouiti.me>
2015-10-14 15:48:26 +0200

    Add filter

--- db/migrate/20151002141853_studentsbook2.rb
+++ db/migrate/20151002141853_studentsbook2.rb
@@ -1,5 +1,5 @@
 class Studentsbook2 < ActiveRecord::Migration
   def change
-    add_column :users_infos, :graduation_year, :integer,:limit => 4
+    add_column :users_infos, :graduation_year, :integer,:limit => 4, :default => 0
   end
 end
