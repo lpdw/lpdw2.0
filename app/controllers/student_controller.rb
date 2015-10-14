f3289b01b8123f20c3a212c0928e272a36549a7a
Laouiti Elias Cédric <elias@laouiti.me>
2015-10-14 16:24:39 +0200

    Edit student controller

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,5 +1,7 @@
 class StudentController < ApplicationController
   def show
+    @graduationYears = Student.select("graduation_year").all
+
     if(params[:user_id] == nil)
       @student = Student.joins("LEFT JOIN users ON users.id = users_infos.user_id").select("*").where("role='student' AND graduation_year=0")
     else
