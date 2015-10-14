1197647fd91e91b7e60a5feec19c21d35a45baa1
Laouiti Elias Cédric <elias@laouiti.me>
2015-10-14 18:25:43 +0200

    Fix commit fail

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,11 +1,10 @@
 class StudentController < ApplicationController
   def show
-    @graduationYears = Student.select("graduation_year").order("graduation_year").all
-
-    if(params[:user_id] == nil)
-      @student = User.students.where("graduation_year=0")
+    @graduationYears = UsersInfo.select("graduation_year").order("graduation_year").all
+    if(params[:graduation_years] == nil)
+      @student = User.students.select {|student| student.users_info.graduation_year == 0}
     else
-      @student = User.students.where("graduation_year=%s", params[:user_id])
+      @student = User.students.select {|student| student.users_info.graduation_year == params[:graduation_years].to_i}
     end
   end
 
