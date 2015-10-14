846e67eb0458eecdc00aaa0ef497ef9f4c95d2ed
Maxime Florile <florile.maxime@gmail.com>
2015-10-14 17:23:38 +0200

    update_average_salary

---
+++ db/migrate/20151014151107_update_average_salary.rb
@@ -0,0 +1,7 @@
+class UpdateAverageSalary < ActiveRecord::Migration
+  def change
+    drop_table :average_salaries
+    add_column :users_infos, :current_average_salary, :string
+    add_column :users_infos, :lp_average_salary, :string
+  end
+end
