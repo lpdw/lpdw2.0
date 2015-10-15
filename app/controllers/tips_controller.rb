87f8c018bbf8783c29ff2ef470282e498536a911
MatthieuCt <cayet.matthieu@gmail.com>
2015-10-15 11:23:08 +0200

    Affichage des bons plans de Tip avec leur categorie

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,11 +1,18 @@
 class TipsController < ApplicationController
 
-  def show
-    @tip = Tip.find_by(params[:id])
-  end
+    def show
+        @tip = Tip.find_by(params[:id])
+    end
 
-  def index
-    @tips = Tip.all
-  end
+    def index
+        #Permet de récupérer tous les différents types de Tip
+        @diff_category = Tip.uniq.pluck(:type)
+        @category = params[:category]
+        if @category 
+            @tips = Tip.where(type: @category)
+        else
+            @tips = Tip.all
+        end
+    end
 
 end
