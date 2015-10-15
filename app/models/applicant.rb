834fe2cd2c07b114d3fdede34c46ef0d93952ea1
Yassin AGHANI <aghani.yassin@gmail.com>
2015-10-15 22:49:56 +0200

    Fix bug Mysql select by year

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -19,7 +19,16 @@ class Applicant < ActiveRecord::Base
   accepts_nested_attributes_for :project_applicants, :allow_destroy => true
   accepts_nested_attributes_for :applicant_status, :votes
 
-  scope :by_year, lambda { |year| where("strftime('%Y', created_at) = ?", year.to_s) }
+  #scope :by_year, lambda { |year| where("strftime('%Y', created_at) = ?", year.to_s) }
+  scope :by_year, lambda { |year|
+    adapter_type = connection.adapter_name.downcase.to_sym
+    case adapter_type
+    when :mysql
+      where('extract(year from created_at) = ?', year)
+    when :sqlite
+      where("strftime('%Y', created_at) = ?", year.to_s)
+    end
+  }
 
   after_create :set_applicant_status
 
