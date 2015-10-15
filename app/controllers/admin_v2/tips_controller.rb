d3e752c2e013fd900eb13a7f7d019de115f82c1a
Mangubu <mangubu@MacBook-Air-de-Jean-David.local>
2015-10-15 15:12:46 +0200

    update admin tips controller

--- app/controllers/admin_v2/tips_controller.rb
+++ app/controllers/admin_v2/tips_controller.rb
@@ -1,54 +1,53 @@
-class AdminV2::TipsController <  AdminV2Controller
+class AdminV2::TipsController < AdminV2Controller
+  before_action :set_tip, only: [:edit, :update, :destroy]
 
-	def index
-		@tips = Tip.all
-	end
+  def index
+    @tips = Tip.all
+  end
 
-	def new
-		@tip = Tip.new
-	end
-
-	def create
-		@tip = Tip.new(tip_params)
-	    if @tip.save
-	      flash['sucess'] = 'Tip created'
-        redirect_to url_for(:controler => :TipsController, :action => :new)
-	    else
-	      flash['fail'] = 'Tip not created'
-	      redirect_to url_for(:controler => :TipsController, :action => :new)
-    	end
-	end
+  def new
+    @tip = Tip.new
+  end
+  def create
+    @tip = Tip.new(tip_params)
+    if @tip.save
+      flash['sucess'] = 'Tip created'
+    else
+      flash['fail'] = 'Tip not created'
+    end
+    redirect_to admin_v2_tips_path
+  end
 
   def edit
-    @tip = Tip.find(params[:id])
   end
 
   def update
-    @tip = Tip.find(params[:id])
     @tip.update_attributes(tip_params)
     if @tip.save
       flash['sucess'] = 'Tip updated'
-      redirect_to url_for(:controler => :TipsController, :action => :index)
     else
       flash['fail'] = 'Tip not updated'
-      redirect_to url_for(:controler => :TipsController, :action => :index)
+    end
+    redirect_to admin_v2_tips_path
+  end
+
+  def destroy
+    if @tip.destroy
+      flash['sucess'] = 'Tip delete'
+      redirect_to admin_v2_tips_path
+    else
+      flash['fail'] = 'Tip not deleted'
+      redirect_to admin_v2_tips_path
     end
   end
 
-	def destroy
-		@tip = Tip.find(params[:id])
-    	if @tip.destroy
-      		flash['sucess'] = 'Tip delete'
-      		redirect_to admin_v2_tips_path
-    	else
-      		flash['fail'] = 'Tip not deleted'
-      		redirect_to admin_v2_tips_path
-    	end
-  	end
+  private
 
-	private
+  def set_tip
+    @tip = Tip.find(params[:id])
+  end
 
-	def tip_params
-	  	params[:tip].permit(:title, :description, :category, :end_at, :url, :phone, :address)
-  	end
+  def tip_params
+    params[:tip].permit(:title, :description, :category, :end_at, :url, :phone, :address)
+  end
 end
