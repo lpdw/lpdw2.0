d6f2ac7f2b0cae97f28bf3ff16ed7abd4687857f
Laouiti Elias Cédric <elias@laouiti.me>
2015-10-16 13:03:20 +0200

    Fix style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005214752) do
+ActiveRecord::Schema.define(version: 20151016093438) do
 
   create_table "actualities", force: true do |t|
     t.string   "title"
@@ -233,7 +233,7 @@ ActiveRecord::Schema.define(version: 20151005214752) do
     t.string   "facebook"
     t.string   "github"
     t.string   "googleplus"
-    t.datetime "birth"
+    t.date     "birth"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
