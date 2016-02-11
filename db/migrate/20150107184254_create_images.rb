e312d51e338bb18a515f4ba25f8276ea8c97a5e9
Yassin Aghani <aghani.yassin@gmail.com>
2016-02-11 19:17:51 +0100

    Fix migration for production

--- db/migrate/20150107184254_create_images.rb
+++ db/migrate/20150107184254_create_images.rb
@@ -1,10 +1,10 @@
 class CreateImages < ActiveRecord::Migration
   def change
-    create_table :images do |t|
-      t.string :alt
-      t.string :attachment
-
-      t.timestamps
-    end
+    #create_table :images do |t|
+    #  t.string :alt
+    #  t.string :attachment
+    #
+    #  t.timestamps
+    #end
   end
 end
