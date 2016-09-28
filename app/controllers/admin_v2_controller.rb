6573f4eef6293de00cf4d375f7b9fcb37be53db1
Léo Riazan <leo.riazan@hotmail.fr>
2016-09-28 17:10:30 +0200

    Ajout de byebug dans le Gemfile et lien actif

--- app/controllers/admin_v2_controller.rb
+++ app/controllers/admin_v2_controller.rb
@@ -1,7 +1,6 @@
 class AdminV2Controller < ApplicationController
   layout '_admin'
   before_action :authenticate_user!
-
   def authorize
     redirect_to(root_path) unless current_user.try(:admin?)
   end
