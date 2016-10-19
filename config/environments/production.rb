c0087ad79001db027d581e03d776a6675991b1ea
Sonny-cole Naomie <sonny-cole@iMac-de-assistance-2.local>
2016-10-19 11:36:11 +0200

    Mot de passe oublier
    Please enter the commit message for your changes. Lines starting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 end
-
 
